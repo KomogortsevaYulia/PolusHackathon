1c994332ec2981fa3df54be8fbb6570d15b0152b
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-10-15 21:12:08 +0800

    +

--- client/src/pages/AddRequestPage/AddRequestPage.jsx
+++ client/src/pages/AddRequestPage/AddRequestPage.jsx
@@ -328,17 +328,7 @@ const AddRequestPage = () => {
                 style={{ width: "100%", height: "50vh", borderRadius: "25px" }}
               ></div>
             </div>
-          </div>
-          <div className="row">
-            <div className="card" style="width: 18rem;">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
-              </div>
-            </div>
+          
           </div>
           <div className="row">
             <div class="mt-3 row justify-content-md-center" onClick={onSubmit}>
